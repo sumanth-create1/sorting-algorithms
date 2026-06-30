02375a526bc33a15d9cbd30e21bca33ea7c26d8b
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-06-30 20:58:47 +0530

    implemented BubbleSort

---
+++ bubblesort.java
@@ -0,0 +1,24 @@
+public class bubblesort {
+    static void bubbleSort(int [] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n-1;i++) {
+            for(int j = 0; j < n-1-i;j++) {
+                if(nums[j] > nums[j+1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] main) {
+        int [] nums = {5,1,2,3,4};
+
+        bubbleSort(nums);
+
+        for(int x:nums) {
+            System.out.print(x + " ");
+        }
+    }
+}
