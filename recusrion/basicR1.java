2256da1d176169cbf537d87b6f1a2f9ab3630b0b
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-07-01 09:56:06 +0530

    implemented printing numbers using recursion

--- recusrion/basicR1.java
+++ recusrion/basicR1.java
@@ -10,8 +10,19 @@ public class basicR1 {
         print(n - 1);
     }
 
+    //print numbers from 1 to 5........
+
+    public static void printNum(int n) {
+        if(n==6) {
+            return;
+        }
+
+        System.out.println(n);
+        printNum(n+1);
+    }
+
     public static void main(String[] args) {
-        int n = 5;
-       print(n);
+        int n = 1;
+       printNum(n);
     }
 }
