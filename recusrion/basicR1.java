2453962c060f9808c9f64854c960034e64338d30
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-07-01 09:33:30 +0530

    started basic recursion

---
+++ recusrion/basicR1.java
@@ -0,0 +1,17 @@
+package recusrion;
+
+public class basicR1 {
+    public static  void print(int n) {
+
+        if (n == 0)
+            return;
+
+        System.out.println(n);
+        print(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+       print(n);
+    }
+}
